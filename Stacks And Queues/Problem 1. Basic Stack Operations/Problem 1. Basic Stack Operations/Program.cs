b0b9dee5026a0980f0514b32587aff65535ed8df
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-29 18:25:15 +0200

    Basic Stack Operations

---
+++ Stacks And Queues/Problem 1. Basic Stack Operations/Problem 1. Basic Stack Operations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numberToPush = input[0];
+            int numberToPop = input[1];
+            int numberToContain = input[2];
+
+            var inputNumberToPush = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            var stack = new Stack<int>(inputNumberToPush);
+
+            for (int i = 0; i < numberToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(numberToContain))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
