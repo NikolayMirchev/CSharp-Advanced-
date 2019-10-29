914afc5bb346a6a97a82dd5bbd1d87e2f3f0c94e
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-29 18:27:35 +0200

    Basic Queue Operations

---
+++ Stacks And Queues/Problem 2. Basic Queue Operations/Problem 2. Basic Queue Operations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_2._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputIntegers = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int numbersToRemove = inputIntegers[1];
+            int numberToContains = inputIntegers[2];
+
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>(input);
+
+            for (int i = 0; i < numbersToRemove; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(numberToContains))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
