97b10bb0b101b208da10426751269daecde55b30
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-29 19:39:31 +0200

    Balanced parentheses

---
+++ Stacks And Queues/Problem 8.  Balanced Parentheses/Problem 8.  Balanced Parentheses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_8.__Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+            bool isBalanced = true;
+            bool turn = false;
+
+            foreach (var character in input)
+            {
+                if (character == '{' || character == '(' || character == '[' || character == ' ' && !turn)
+                {
+                    stack.Push(character);
+                }
+                else if (character == '}' || character == ')' || character == ']' || character == ' ')
+                {
+                    turn = true;
+                    if (character == '}' && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else if (character == ')' && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else if (character == ']' && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else if (character == ' ' && stack.Peek() == ' ')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
