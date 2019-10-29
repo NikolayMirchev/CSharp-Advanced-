e5cb63509063d831ca858b4bb79e1f890df4397a
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-29 16:20:00 +0200

    Stack Sum

---
+++ Stacks And Queues/2. Stack Sum/2. Stack Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] inputNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            foreach (var num in inputNumbers)
+            {
+                stack.Push(num);
+            }
+
+            string command = Console.ReadLine();
+            while (command.ToLower() != "end")
+            {
+                string[] input = command.Split(" ").ToArray();
+                if (input[0].ToLower() == "add")
+                {
+                    stack.Push(int.Parse(input[1]));
+                    stack.Push(int.Parse(input[2]));
+                }
+                else if (input[0].ToLower() == "remove")
+                {
+                    if (stack.Count > int.Parse(input[1]))
+                    {
+                        for (int i = 0; i < int.Parse(input[1]); i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            int totalSum = 0;
+            while (stack.Count > 0)
+            {
+                totalSum += stack.Pop();
+            }
+
+            Console.WriteLine($"Sum: {totalSum}");
+            
+        }
+    }
+}
