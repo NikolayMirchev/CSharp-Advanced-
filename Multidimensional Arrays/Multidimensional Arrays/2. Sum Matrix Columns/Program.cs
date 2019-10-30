727f2398818b6fb3d3b7814cbb84eb9dcb433c22
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-30 15:03:31 +0200

    Matrix

---
+++ Multidimensional Arrays/Multidimensional Arrays/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dim = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] arr = new int[dim[0], dim[1]];
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = tokens[j];
+                }
+            }
+
+            for (int col = 0; col < arr.GetLength(1); col++)
+            {
+                for (int row = 0; row < arr.GetLength(0); row++)
+                {
+                    sum += arr[row, col];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
