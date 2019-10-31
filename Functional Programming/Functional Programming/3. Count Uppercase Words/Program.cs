cdaa6178cf88dd220e7987996adc74eee2bce762
NikolayMirchev <mirchev.aria@gmail.com>
2019-10-31 13:26:38 +0200

    Functional Programming Lab

---
+++ Functional Programming/Functional Programming/3. Count Uppercase Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = w => //w[0] == w.ToUpper()[0];
+            {
+                if (w[0] == w.ToUpper()[0])
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            var arr = Console.ReadLine()
+                .Split()
+                .Where(func)
+                .ToArray();
+
+            foreach (var word in arr)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Functional Programming/Functional Programming/5. Filter by Age/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Filter_by_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var people = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(", ")
+                    .ToArray();
+
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+
+                people[name] = age;
+            }
+
+            string olderOrYounger = Console.ReadLine();
+            int ages = int.Parse(Console.ReadLine());
+
+            foreach (var kvp in people.Where(kvp => olderOrYounger == "older" ? kvp.Value > ages : kvp.Value < ages))
+            {
+                Console.WriteLine($"{kvp.Key}");
+            }
+
+        }
+    }
+}
